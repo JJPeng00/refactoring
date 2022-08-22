48b15a245296c57ec9e295295824da47eb7d1ddc
JJPeng <pengjiajun755@163.com>
2022-08-22 12:50:31 +0800

    ch6-提取方法，重构：局部变量再赋值

--- src/ch6/ExtractMethodDemo.java
+++ src/ch6/ExtractMethodDemo.java
@@ -15,6 +15,8 @@ import java.util.Date;
  * 1. 提取无局部变量的方法
  * 2. 提取有局部变量的方法，当局部变量只被读取时，可简单的直接将局部变量作为参数传入，idea会智能的帮助我们直接生成一个带有这两个参数的方法
  * 3. 提取有局部变量的方法，当局部变量是一个数据结构（对象），且在提取的方法中只修改这个结构中的数据时，也可简单的直接将局部变量作为参数传入
+ * 4. 对局部变量再赋值，当局部变量只在被提炼的代码中被使用是，可直接将该变量的声明也移动到提取的代码中去，
+ *    如果局部变量还被提炼之外的代码，可以将修改后的局部变量作为结果返回
  */
 public class ExtractMethodDemo {
 
@@ -24,18 +26,23 @@ public class ExtractMethodDemo {
     }
 
     private static void printOwing(Invoice invoice) {
-        int outstanding = 0;
+
         printBanner();
 
-        // calculate outstanding
-        for (final Order order : invoice.orders) {
-            outstanding += order.amount;
-        }
+        final int outstanding = calculateOutstanding(invoice);
 
         recordDueDate(invoice);
 
         printDetails(invoice, outstanding);
 
+    }
+
+    private static int calculateOutstanding(Invoice invoice) {
+        int result = 0;
+        for (final Order order : invoice.orders) {
+            result += order.amount;
+        }
+        return result;
     }
 
     private static void recordDueDate(Invoice invoice) {
