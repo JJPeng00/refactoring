7bc881229f6a1e9256e2ed1400b41e268cfa4872
JJPeng <pengjiajun755@163.com>
2022-08-22 12:22:11 +0800

    ch6-提取方法，重构：将打印横幅的代码提取出来，这段代码没有局部变量，是最简单的提取方法

--- src/ch6/ExtractMethodDemo.java
+++ src/ch6/ExtractMethodDemo.java
@@ -9,6 +9,10 @@ import java.util.Date;
  *
  * 时机：将不同作用的代码段分别提炼出来，即使代码只有一行，我们将不再关心方法如何实现，只关心方法的作用。
  *      如果之后发现提炼的方法并不合适可以将方法内联回去。
+ *
+ * idea中选中某段代码后使用 Ctrl+Alt+M快捷键可将选中的代码提取为一个方法
+ *
+ * 1. 提取无局部变量的方法
  */
 public class ExtractMethodDemo {
 
@@ -19,9 +23,7 @@ public class ExtractMethodDemo {
 
     private static void printOwing(Invoice invoice) {
         int outstanding = 0;
-        System.out.println("***********************");
-        System.out.println("**** Customer Owes ****");
-        System.out.println("***********************");
+        printBanner();
 
         // calculate outstanding
         for (final Order order : invoice.orders) {
@@ -37,6 +39,12 @@ public class ExtractMethodDemo {
         System.out.println("amount: " + outstanding);
         System.out.println("due: " + invoice.dueDate);
 
+    }
+
+    private static void printBanner() {
+        System.out.println("***********************");
+        System.out.println("**** Customer Owes ****");
+        System.out.println("***********************");
     }
 
 }
